3b50850f8b6c97364b84358b44c63c2f858a8f4f
ABDALLAH Karim <karim.abdallah@etu.univ-amu.fr>
2023-05-26 11:27:09 +0200

    First commit of the repository abt the function of Draw the Hangaman

---
+++ main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+
+using namespace std;
+
+void PrintMessage(string message, bool printTop = true, bool printBottom = true)
+{
+    if (printTop)
+    {
+        cout << "+---------------------------------+" << endl;
+        cout << "|";
+    }
+    else
+    {
+        cout << "|";
+    }
+    bool front = true;
+    for (int i = message.length(); i<33; i++) 
+    {
+        if (front)
+        {
+            message = " " + message;
+        }
+        else 
+        {
+            message = message +" ";
+        }
+        front = !front;
+    }
+    cout << message.c_str();
+
+    if (printBottom)
+    {
+        cout << "|"<<endl;
+        cout << "+---------------------------------+" << endl;
+    }
+    else
+    {
+        cout << "|" << endl;
+    }
+
+}
+
+void DrawHangMan(int guessCount = 0)
+{
+    if (guessCount >= 1)
+        PrintMessage("|", false, false);
+    else
+        PrintMessage("", false, false);
+ 
+    if (guessCount >= 2)
+        PrintMessage("|", false, false);
+    else
+        PrintMessage("", false, false);
+ 
+    if (guessCount >= 3)
+        PrintMessage("O", false, false);
+    else
+        PrintMessage("", false, false);
+ 
+    if (guessCount == 4)
+        PrintMessage("/  ", false, false);
+    
+    if (guessCount == 5)
+        PrintMessage("/| ", false, false);
+ 
+    if (guessCount >= 6)
+        PrintMessage("/|\\", false, false);
+    else
+        PrintMessage("", false, false);
+ 
+    if (guessCount >= 7)
+        PrintMessage("|", false, false);
+    else
+        PrintMessage("", false, false);
+ 
+    if (guessCount == 8)
+        PrintMessage("/", false, false);
+ 
+    if (guessCount >= 9)
+        PrintMessage("/ \\", false, false);
+    else
+        PrintMessage("", false, false);
+}
+
+int main()
+{
+    PrintMessage("Le jeu du pendu");
+    DrawHangMan(9);
+    getchar();
+    return 0;
+}
